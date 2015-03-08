1fa8f66bfb28e336cb252022ee3843453e2b4601
Ian Macalinao <me@ian.pw>
2015-03-08 14:54:24 -0500

    Finish degree planning algorithm

--- plan_degree.js
+++ plan_degree.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 var courses = require('./courses');
 
 module.exports = function planDegree(taken, pending, load) {
@@ -21,5 +22,20 @@ module.exports = function planDegree(taken, pending, load) {
     });
 
     // Find hours and put them in
+    var remaining = load;
+    var sem = [];
+    eligible = _.shuffle(eligible);
+    while (eligible.length > 0 && remaining > 0) {
+      var next = eligible.pop();
+      if (remaining - next.hours > 0) {
+        sem.push(next.name);
+      }
+    }
+
+    // Add to plan
+    plan.push(sem);
   }
+
+  // Return
+  return plan;
 };
