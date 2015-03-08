0b50ca4056af49868dcc6556de8560de3731c8bf
Ian Macalinao <me@ian.pw>
2015-03-07 18:56:59 -0600

    Add port

--- index.js
+++ index.js
@@ -30,4 +30,9 @@ module.exports = {
   findCourses: findCourses
 };
 
+var app = express();
 
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
