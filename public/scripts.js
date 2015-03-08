45ff50edc3731cf7221cd022a24b21705eed914a
Ian Macalinao <me@ian.pw>
2015-03-08 15:35:05 -0500

    Add test invalid class name

--- public/scripts.js
+++ public/scripts.js
@@ -14,4 +14,17 @@ angular.module('app', ['ui.router'])
 
 .controller('HomeCtrl', function($scope) {
   $scope.test = 1;
+
+  $scope.taken = [];
+
+  $scope.addTaken = function() {
+    if ($scope.taken.indexOf($scope.takenName) !== -1) {
+      alert('Class already added!');
+    } else if (!/^[A-Z]+ [\d]{4}$/.test($scope.takenName)) {
+      alert('Invalid class name!');
+    } else {
+      $scope.taken.push($scope.takenName);
+    }
+    $scope.takenName = '';
+  };
 });
