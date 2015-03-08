aea236c59ed83892b6bcf5c26a9eef612e85d7e4
Ian Macalinao <me@ian.pw>
2015-03-08 14:30:58 -0500

    Move data to data dir

--- flatten.js
+++ flatten.js
@@ -1,7 +1,34 @@
 var fs = require('fs');
-var courses = JSON.parse(fs.readFileSync('courses.json').toString());
+var courses = JSON.parse(fs.readFileSync('data/courses.json').toString());
 console.log('Flattening courses...');
-fs.writeFileSync('courses.flat.json', JSON.stringify(courses.reduce(function(a, b) {
+courses = courses.reduce(function(a, b) {
   return a.concat(b);
-})));
+});
 console.log('Flattened.');
+console.log('Fixing bad course names...');
+courses = courses.map(function(course) {
+
+  function listFixNames(list) {
+    if (!list) return [];
+    return list.map(function(name) {
+      return (/([A-Z]+ [\d]{4})/.exec(name) || [])[1];
+    }).filter(function(name) {
+      return /([A-Z]+ [\d]{4})/.test(name);
+    });
+  }
+
+  function fixNames(list) {
+    if (!list) return [];
+    return list.map(listFixNames);
+  }
+
+  course.prereqs = fixNames(course.prereqs);
+  course.coreqs = fixNames(course.coreqs);
+  course.sameAs = listFixNames(course.sameAs);
+
+  return course;
+});
+console.log('Fixed.');
+console.log('Writing...');
+fs.writeFileSync('data/courses.flat.json', JSON.stringify(courses));
+console.log('Written.');
