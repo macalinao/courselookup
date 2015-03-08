ac8461dad0db73771fd46b4b00290deabe770f8d
Ian Macalinao <me@ian.pw>
2015-03-08 15:14:30 -0500

    Add plan

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 
+var plan = require('./plan_degree');
+
 var app = express();
+
+app.use(require('body-parser').json());
+
+app.post('/plan', function(req, res) {
+  res.json(plan(req.body.taken, req.body.pending, req.body.load));
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
