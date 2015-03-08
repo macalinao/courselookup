2c02f6474ecd779357b03e71c4dd159b2c158da3
Ian Macalinao <me@ian.pw>
2015-03-07 19:14:39 -0600

    Remove parens

--- index.js
+++ index.js
@@ -9,17 +9,40 @@ function findCourses(dept) {
     var $ = cheerio.load(res.text);
     var ret = [];
     $('p.course-catalog-description').each(function() {
-      console.log('asf');
       var $t = $(this);
       var name = $($t.find('.course_address a')[0]).text();
       var title = $($t.find('.course_title')[0]).text();
       var hours = parseInt($($t.find('.course_hours')[0]).text().substring(1, 2));
       var desc = $t.clone().children('span').remove().end().text().trim();
+
+      // Parse prerequisites
+      var descs = desc.split('.').map(function(el) {
+        return el.trim();
+      });
+
+      var prereq = (descs.filter(function(el) {
+        return el.indexOf('Prerequisite') === 0;
+      }) || [])[0];
+      if (prereq) {
+        var prereqs = prereq.substring(prereq.indexOf(':') + 2).split(' and ').map(function(el) {
+          while (el.indexOf('(') === 0) {
+            var close = el.indexOf(')');
+            if (close === -1) {
+              el = el.substring(1);
+            } else {
+              el = el.substring(1, close);
+            }
+          }
+          return el;
+        });
+      }
+
       ret.push({
         name: name,
         title: title,
         hours: hours,
-        desc: desc
+        desc: desc,
+        prereqs: prereqs
       });
     });
     return ret;
