051e0e365ccaf8aa1ad9fd183e9c81e87811fbea
Ian Macalinao <me@ian.pw>
2015-04-20 01:35:21 -0500

    Add following adjacency list

--- test/helpers/create_adjacency_list.js
+++ test/helpers/create_adjacency_list.js
@@ -17,6 +17,24 @@ describe('create adjacency list', () => {
     expect(list['CS 2336'].adjacent[0].also)
       .to.include.members(['CS 2336', 'CE 2336', 'TE 2336']);
 
+    expect(list['CS 1337']).to.be.undefined;
+
+  });
+
+  it('should create an accurate followed adjacency list', () => {
+
+    let list = createAdjacencyList(['CS 3345'], true);
+
+    expect(list['CS 2305'].adjacent[0].name).to.equal('CS 3345');
+    expect(list['CS 2305'].adjacent[0].also)
+      .to.include.members(['CS 2305', 'CE 2305', 'TE 2305']);
+
+    expect(list['CS 2336'].adjacent[0].name).to.equal('CS 3345');
+    expect(list['CS 2336'].adjacent[0].also)
+      .to.include.members(['CS 2336', 'CE 2336', 'TE 2336']);
+
+    expect(list['CS 1337']).to.not.be.undefined;
+
   });
 
 });
